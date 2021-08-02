0ebdee7031c27dc272da8c0e696c62206284215e
Abdul Sami <abdulsamisiddiq2001@gmail.com>
2021-08-03 02:23:36 +0500

    7 chapters practice

--- Basic JS/script.js
+++ Basic JS/script.js
@@ -1,1 +1,58 @@
 console.log("Hope, I'll be consistent and will try to enhance my skill time by time, InShaAllah :) ")
+
+// chap 1 //
+// alert("AlertBox!")
+// chap 1 //
+
+
+// chap 2 //
+
+var name1 = "Abdul Sami";
+// alert(name1);
+
+// chap 2 //
+
+
+// chap 3 //
+
+var eg = 144; // int itSelf, but alert convert it into string!, this is on book, but now at aug 21 its not working any more
+
+// alert(eg + 4)
+// alert(typeof(eg));
+
+// chap 3 //
+
+// chap 4 //
+    // var $abc = 4+3;
+    // console.log($abc)
+// chap 4 //
+
+
+// chap 5 //
+    // var $abc = 4+3;
+    // var $opq = 15-61;
+    // console.log($abc+$opq);
+// chap 5 //
+
+// chap 6 //
+
+    var num  = 15;
+    // console.log(num) // 1
+    // var newNum = num++;
+    // console.log(newNum) // 1
+    // console.log(num) // 2
+
+    console.log(num++) // 15
+    console.log(--num) // 15
+    console.log(--num) // 14
+    console.log(++num) // 15
+    console.log(num++) // 15
+    console.log(--num) // 15
+    console.log(num--) // 15
+    console.log(num++)// 14
+    console.log(num--)// 15
+    console.log(++num) // 15
+    console.log(--num) // 14
+    console.log(--num) // 13
+
+// chap 6 //
