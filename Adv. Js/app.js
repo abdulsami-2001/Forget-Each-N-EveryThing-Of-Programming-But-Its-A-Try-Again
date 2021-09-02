a411e1d1741774061be781f33bd221693fd44539
Abdul Sami <abdulsamisiddiq2001@gmail.com>
2021-09-03 03:05:03 +0500

    2-3 Sept Repractice
    Practicing for enchaning skilss

---
+++ Adv. Js/app.js
@@ -0,0 +1,83 @@
+// Var, let, const //
+
+
+    // var a = 10;
+    // console.log(a)
+    // let a = 20;
+    // console.log(a)
+    
+    
+    /*
+    function myLet(){
+        let a = 10;
+        if(true){
+             a = 20;
+            console.log(a)// 20
+        }
+        console.log(a) //20
+    }
+    myLet()
+    */
+
+    /*
+    function myLet(){
+        let a = 10;
+        if(true){
+            let a = 20;
+            console.log(a) //20
+        }
+        console.log(a) // 10
+    }
+    myLet()
+   */
+
+    // for (let i = 0; i < 5; i++) {
+    //     console.log(i)
+    // }
+
+    // console.log(i) // not in the scope, but accessable in var scenario
+
+
+// Var, let, const //
+
+
+
+// Variables Hoisting //
+    /*
+        a = 50;
+        console.log(a)
+        var a; // works, but if let a; then it can't
+    */
+    /*
+        var i = "hello";
+        console.log(i)
+        function myFunc(){ 
+            // var i; due to hoisting
+            console.log(i) // undefined
+            var i = 3
+            console.log(i) // 3
+        }
+
+        myFunc();
+    */
+// Variables Hoisting //
+
+
+// Arrow Function //
+
+
+// myFunc()
+// const myFunc = () => console.log("Hello")
+
+// const myFunc = a =>  a*5;
+// console.log(myFunc(6))
+
+
+// const myFunc = a =>  {return a*5};
+// console.log(myFunc(8))
+
+// const myFunc = (a,...b) =>  b[1];
+// console.log(myFunc(10,5,6,9,6,6))
+
+
+// Arrow Function //
