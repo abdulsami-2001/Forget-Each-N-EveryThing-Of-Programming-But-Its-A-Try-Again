8d4c6ff6037afdd98b23565c18c839c7bb703b2d
Abdul Sami <abdulsamisiddiq2001@gmail.com>
2021-09-08 03:52:28 +0500

    Till 26
    Theory thi, isliye nhi ha refletion ziyada or new file create nhi hwi ha,
    
    Well, phases of comp are mounting, updating error handling and unmounting.
    
    Mounting: Constructor,( ) getDerivedStateFromProps( ), render( ) and componentDidMount( ).
    
    Updating:  getDerivedStateFromProps( ), shouldComponentUpdate( ), render( ), getSnapshotBeforeUpdate( ) optional and componentDidUpdate( ).
    
    Unmounting: componentWillUnmount( ).

--- React-JS/all-tut-prac-again/src/SetState 23/SetStateFunc.component.jsx
+++ React-JS/all-tut-prac-again/src/SetState 23/SetStateFunc.component.jsx
@@ -4,9 +4,11 @@ class SetStateFunc extends Component{
     
     constructor(props){
         super(props)
+        let mapen = "ABC" 
         this.state={
             myName: "Sam",
-            rollNo: this.props.rollNo
+            rollNo: this.props.rollNo,
+            dummy: mapen
         }
     }
     
@@ -28,12 +30,15 @@ class SetStateFunc extends Component{
     
     render(){
         console.log(this.state)
+        let s = "SAmaa"
         return(
             <React.Fragment>
                 <h1>Hello, Set State Using NoFunction In Set State Method</h1>
                 <h1>Name Is: {this.state.myName}</h1>
                 <h1>Roll No Is: {this.state.rollNo}</h1>
                 <h1>Name2 Is: {this.state.myName2}</h1>
+                <h3>Dummy: {this.state.dummy}</h3>
+                <h4>{s}</h4>
                 <button onClick={()=>{this.onClickHandle()}}>Update State</button>
             </React.Fragment>
         )
